dc138220a2fd7f8cfebd5d5ae9f6104c029dae7a
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-11-15 19:53:09 +0530

    final touches done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 import AddTask from './components/AddTask';
 import TaskList from './components/TaskList';
-import { Button, Container, Modal } from 'react-bootstrap';
+import { Button, Container, Modal, Alert } from 'react-bootstrap';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([]);
+  const [showAlert, setShowAlert] = useState(true); // State for alert visibility
   const SERVER_URL = 'http://localhost:8080/api/tasks';
 
   useEffect(() => {
@@ -30,7 +31,6 @@ function App() {
 
   const handleClose = () => {
     setShowAddTask(false);
-    // Refetch tasks after closing the modal to ensure consistency
     const fetchTasks = async () => {
       try {
         const response = await axios.get(SERVER_URL);
@@ -46,7 +46,6 @@ function App() {
     try {
       const response = await axios.post(SERVER_URL, task);
       setTasks([...tasks, response.data]);
-      // Close the modal after successful addition
       handleClose();
     } catch (error) {
       console.error('Error adding task:', error);
@@ -66,6 +65,14 @@ function App() {
         padding: '2rem'
       }}
     >
+      {showAlert && (
+        <Alert variant="warning" onClose={() => setShowAlert(false)} dismissible>
+          <Alert.Heading>Notice</Alert.Heading>
+          <p>
+            Please note that there might be a 50s downtime while interacting with the backend in the free trial of render.
+          </p>
+        </Alert>
+      )}
       <h2 className="text-center mb-4" style={{ color: '#2c3e50', fontSize: '2.5rem', fontWeight: 'bold' }}>Task Management</h2>
       <Button 
         variant="primary" 
